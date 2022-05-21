b83ca3108a59060888aac0c3609564b5e151d37d
f0rkz <f0rkz@f0rkznet.net>
2022-05-21 02:25:37 -0400

    Update docker settings

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,4 +4,4 @@
 rm -r /tmp/.X0-lock
 cd /root/.wine/drive_c/VRisingServer/
 Xvfb :0 -screen 0 1024x768x16 & \
-DISPLAY=:0.0 wine VRisingServer.exe
+DISPLAY=:0.0 wine VRisingServer.exe -persistentDataPath .\save-data -serverName "${SERVERNAME}" -saveName "world1" -logFile ".\logs\VRisingServer.log"
